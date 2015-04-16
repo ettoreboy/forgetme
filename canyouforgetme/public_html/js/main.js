12cd7c222d527d5b17d1ec595e3a6ce8c8d3f5f2
ettore <cipettaro@gmail.com>
2015-04-16 12:45:24 +0300

    nb project started

---
+++ canyouforgetme/public_html/js/main.js
@@ -0,0 +1,11 @@
+
+//simple GET request
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
