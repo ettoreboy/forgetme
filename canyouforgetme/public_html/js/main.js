f23bd548f2a408f7c35ddfa623de0d1dba55d60d
ettore <cipettaro@gmail.com>
2015-04-18 17:35:31 +0300

    fixed js event add

--- canyouforgetme/public_html/js/main.js
+++ canyouforgetme/public_html/js/main.js
@@ -1,3 +1,4 @@
+var PIPL_API="http://api.pipl.com/search/v4/";
 
 //simple GET request
 function httpGet(theUrl)
@@ -5,7 +6,28 @@ function httpGet(theUrl)
     var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
+
+//Add elements to page
+function addElement() {
+    var $newdiv1 = $("<div class='jumbotron' <p> Test </p></div>");
+    $("#results").append($newdiv1);
+    return true;
+}
+
+//Check and submit
+function checkAndSubmit (event) {
+            var email = document.getElementById ("inputEmail");
+            if (email.value.length < 1) {
+                return false;
+            }
+
+            addElement();
+            
+            event.preventDefault();
+            return true;
+        }
+
