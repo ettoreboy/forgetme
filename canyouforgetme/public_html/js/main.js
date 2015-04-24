b70f2658d4c5568123c0bf0a68ae27e16e816427
ettore <cipettaro@gmail.com>
2015-04-24 20:02:23 +0300

    javascript parse of sites completed

--- canyouforgetme/public_html/js/main.js
+++ canyouforgetme/public_html/js/main.js
@@ -1,33 +1,98 @@
-var PIPL_API="http://api.pipl.com/search/v4/";
-
-//simple GET request
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
 
 //Add elements to page
-function addElement() {
-    var $newdiv1 = $("<div class='jumbotron' <p> Test </p></div>");
-    $("#results").append($newdiv1);
+function addElement(content) {
+    var newdiv = document.createElement('div');
+    newdiv.className = "jumbotron";
+    newdiv.innerHTML = content;
+    $("#results").append(newdiv);
     return true;
 }
 
-//Check and submit
-function checkAndSubmit (event) {
-            var email = document.getElementById ("inputEmail");
-            if (email.value.length < 1) {
-                return false;
+//Retrieve accounts informations
+function getSites(url) {
+    $("#results").empty();
+
+    return $.getJSON(url).then(function (data) {
+        var founds = [];
+        if (data.person) {
+            $.each(data.person.urls, function (k, v) {
+
+                $.each(this, function (k, v) {
+                    if (k === "@domain") {
+                        founds.push(v);
+                        console.log(v);
+                    }
+                });
+                ;
+            });
+        } else {
+            addElement("<h2>There are no exact public matches for this email, you are invisible on the Internet!<br> Try to add your name..</h2>");
+        }
+        return founds;
+    })
+            .fail(function () {
+                addElement("<h2>Ups! Somenthing went wrong, try again.</h2>");
+            });
+
+}
+
+
+
+//Update string parameter, if present
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+    else {
+        return uri + separator + key + "=" + value;
+    }
+}
+//Check input and submit
+function checkAndSubmit(event) {
+    event.preventDefault();
+    var email = $('#inputEmail').val();
+    var name = $('#inputName').val();
+    var last = $('#inputLastname').val();
+    if (email === "") {
+
+        return false;
+    }
+
+    var piplAPI = 'http://api.pipl.com/search/v4/' + "?email=" + email + "&key=udbspbnnez3dajrnm4xkrf68" + "&callback=?";
+    if (name) {
+        piplAPI = updateQueryStringParameter(piplAPI, "first_name", name);
+    }
+    if (last) {
+        piplAPI = updateQueryStringParameter(piplAPI, "last_name", last);
+    }
+
+    console.log("Request to: " + piplAPI);
+
+
+    return getSites(piplAPI).then(function (founds) {
+        foundsLength = founds.length;
+        console.log("FOUNDED: " + foundsLength);
+        $.getJSON('sites.json').then(function (sitesdata) {
+            while (foundsLength > 0) {
+                foundsLength--;
+                var current;
+                $.each(sitesdata.domains, function (k, v) {
+
+                    current = founds[foundsLength - 1];
+                    console.log(current);
+                    if (current === v) {
+                        console.log("Match: " + current);
+                    }
+
+                });
+
             }
 
-            addElement();
-            
-            event.preventDefault();
-            return true;
-        }
+
+        });
+
+    });
+}
 
