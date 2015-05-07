b2760b7cf05450dfc7cea97c432431128f1aeb28
ettore <cipettaro@gmail.com>
2015-05-07 13:37:56 +0300

    Working query match

--- canyouforgetme/public_html/js/main.js
+++ canyouforgetme/public_html/js/main.js
@@ -1,10 +1,10 @@
 
 //Add elements to page
-function addElement(content) {
+function addInfoElement(content) {
     var newdiv = document.createElement('div');
-    newdiv.className = "jumbotron";
+    newdiv.className = "panel panel-info";
     newdiv.innerHTML = content;
-    $("#results").append(newdiv);
+    $("#info").append(newdiv);
     return true;
 }
 
@@ -16,27 +16,60 @@ function getSites(url) {
         var founds = [];
         if (data.person) {
             $.each(data.person.urls, function (k, v) {
-
+                var name, domain, url;
                 $.each(this, function (k, v) {
                     if (k === "@domain") {
-                        founds.push(v);
+                        domain = v;
+                        console.log(v + " URL:");
+                    }
+                    if(k === "@name"){
+                        name = v;
+                    }
+                    if (k === "url"){
+                        url = v;
                         console.log(v);
+
                     }
                 });
-                ;
+                founds.push([name, domain, url]);
             });
         } else {
-            addElement("<h2>There are no exact public matches for this email, you are invisible on the Internet!<br> Try to add your name..</h2>");
+            addInfoElement("<h2>There are no exact public matches for this email, you are invisible on the Internet!<br> Try to add your name..</h2>");
         }
         return founds;
     })
             .fail(function () {
-                addElement("<h2>Ups! Somenthing went wrong, try again.</h2>");
+                addInfoElement("<h2>Ups! Somenthing went wrong, try again.</h2>");
             });
 
 }
 
-
+//Create a labelled item depending on the rate
+function addSiteFound (name, domain, rate, info){
+   var newa = document.createElement("a");
+    switch (rate){
+        case "easy":
+        newa.className="list-group-item list-group-success";        
+        break;
+    case "hard":
+        newa.className="list-group-item list-group-item-warning";
+        break;
+    case "impossible":
+        newa.className="list-group-item list-group-item-danger";
+        break;
+    }
+    if(!info){
+        info="No available information about how to delete from this website";
+    }
+    var news = document.createElement("span");
+    news.innerHTML = rate;
+    newa.innerHTML = "<b>"+name+"</b>" + info + news;
+    newa.href = domain ;
+   
+    
+    
+    $("#list-group").append(newa);
+}
 
 //Update string parameter, if present
 function updateQueryStringParameter(uri, key, value) {
@@ -60,7 +93,7 @@ function checkAndSubmit(event) {
         return false;
     }
 
-    var piplAPI = 'http://api.pipl.com/search/v4/' + "?email=" + email + "&key=udbspbnnez3dajrnm4xkrf68" + "&callback=?";
+    var piplAPI = 'http://api.pipl.com/search/v4/' + "?email=" + email + "&key=kdrphm4hv87tab5q34v2sf5n" + "&callback=?";
     if (name) {
         piplAPI = updateQueryStringParameter(piplAPI, "first_name", name);
     }
@@ -69,29 +102,31 @@ function checkAndSubmit(event) {
     }
 
     console.log("Request to: " + piplAPI);
-
-
+    document.getElementById("info")
+    $("#info").empty();
     return getSites(piplAPI).then(function (founds) {
         foundsLength = founds.length;
         console.log("FOUNDED: " + foundsLength);
+        addInfoElement("Founded "+foundsLength+ " site/s registered under "+"<b>"+email+"</b>");
         $.getJSON('sites.json').then(function (sitesdata) {
-            while (foundsLength > 0) {
-                foundsLength--;
-                var current;
-                $.each(sitesdata.domains, function (k, v) {
-
-                    current = founds[foundsLength - 1];
-                    console.log(current);
-                    if (current === v) {
-                        console.log("Match: " + current);
+            var current;
+            $.each(sitesdata, function (k, v) {
+                current = v;
+                $.each(founds, function (k, domainFounded) {
+                    console.log("Checking:" + domainFounded[1] + " = " + current.domains);
+                    if (domainFounded[1] == current.domains) {
+                        console.log(domainFounded[0] +" " + domainFounded[1] + " " + current.difficulty);
+                        addSiteFound(domainFounded[0] +" - "+ domainFounded[1] , current.url, current.difficulty, current.notes);
+                        founds.pop(domainFounded);
+                        return false;
                     }
-
+                    //console.log(v.domains);
                 });
 
-            }
-
-
+            });
         });
+
+
 
     });
 }
