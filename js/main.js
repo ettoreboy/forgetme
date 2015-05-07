a240c9d4913c7d86a698e6f7675ca15916869af7
ettore <cipettaro@gmail.com>
2015-05-07 17:49:28 +0300

    Test site with free key

---
+++ js/main.js
@@ -0,0 +1,130 @@
+
+//Add elements to page
+function addInfoElement(content) {
+    var newdiv = document.createElement('h3');
+    newdiv.innerHTML = content;
+    $(newdiv).hide().appendTo("#info").fadeIn(1000);
+}
+
+//Retrieve accounts informations
+function getSites(url) {
+
+    return $.getJSON(url).then(function (data) {
+        var founds = [];
+        if (data.person) {
+            $.each(data.person.urls, function (k, v) {
+                var name, domain, url;
+                $.each(this, function (k, v) {
+                    if (k === "@domain") {
+                        domain = v;
+                        console.log(v + " URL:");
+                    }
+                    if (k === "@name") {
+                        name = v;
+                    }
+                    if (k === "url") {
+                        url = v;
+                        console.log(v);
+
+                    }                  
+                });
+                founds.push([name, domain, url]);
+            });
+        } else {
+            $(".spinner").hide();
+            addInfoElement("There are no exact public matches for this email! Try to add your name..");
+        }
+        return founds;
+    })
+            .fail(function () {
+                $(".spinner").hide();
+                addInfoElement("Ups! Somenthing went wrong, try again.");
+            });
+
+}
+
+//Create a labelled item depending on the rate
+function addSiteFound(name, domain, rate, info) {
+    var newa = document.createElement("a");
+    switch (rate) {
+        case "easy":
+            newa.className = "list-group-item list-group-success";
+            break;
+        case "hard":
+            newa.className = "list-group-item list-group-item-warning";
+            break;
+        case "impossible":
+            newa.className = "list-group-item list-group-item-danger";
+            break;
+    }
+    if (!info) {
+        info = "No available information about how to delete from this website";
+    }
+    newa.innerHTML = "<b>" + name + "</b> " + info + " <span>RATE: " + rate + "</span>";
+    newa.href = domain;
+    $(newa).hide().appendTo("#list-group").fadeIn(1000);
+
+}
+
+//Update string parameter, if present
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+    else {
+        return uri + separator + key + "=" + value;
+    }
+}
+//Check input and submit
+function checkAndSubmit(event) {
+    event.preventDefault();
+    $("#list-group").empty();
+    $("#info").empty();
+    $(".spinner").show();
+    
+    var email = $('#inputEmail').val();
+    var name = $('#inputName').val();
+    var last = $('#inputLastname').val();
+    if (email === "") {
+        $(".spinner").hide();
+        return false;
+        
+    }
+
+    var piplAPI = 'http://api.pipl.com/search/v4/' + "?email=" + email + "&key=kufrdccc7m2sqqjjdr3kcbzn" + "&callback=?";
+    if (name) {
+        piplAPI = updateQueryStringParameter(piplAPI, "first_name", name);
+    }
+    if (last) {
+        piplAPI = updateQueryStringParameter(piplAPI, "last_name", last);
+    }
+
+    console.log("Request to: " + piplAPI);
+    document.getElementById("info");
+    return getSites(piplAPI).then(function (founds) {
+        foundsLength = founds.length;
+        console.log("FOUNDED: " + foundsLength);
+        addInfoElement("Founded <b>" + foundsLength + " site/s</b> registered under " + "<b>" + email + "</b>");
+        $.getJSON('sites.json').then(function (sitesdata) {
+            var current;
+            $.each(sitesdata, function (k, v) {
+                current = v;
+                $.each(founds, function (k, domainFounded) {
+                    console.log("Checking:" + domainFounded[1] + " = " + current.domains);
+                    if (domainFounded[1] == current.domains) {
+                        console.log(domainFounded[0] + " " + domainFounded[1] + " " + current.difficulty);
+                        addSiteFound(domainFounded[0] + " - " + domainFounded[1], current.url, current.difficulty, current.notes);
+                        founds.pop(domainFounded);
+                        return false;
+                    }
+                    //console.log(v.domains);
+                });
+
+            });
+        });
+        $(".spinner").hide();
+    });
+}
+
