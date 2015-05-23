be59dc604cd0466a2708d5e5101ee813e8048c76
ettore <cipettaro@gmail.com>
2015-05-23 17:21:15 +0300

    Update in pres

--- canyouforgetme/public_html/js/main.js
+++ canyouforgetme/public_html/js/main.js
@@ -1,16 +1,15 @@
-
+var email, last, first;
 //Add elements to page
 function addInfoElement(content) {
-  "use strict";
+    "use strict";
     var newdiv = document.createElement('h3');
     newdiv.innerHTML = content;
-    $("#description").slideDown('slow');
     $(newdiv).hide().appendTo("#info").fadeIn(1000);
 }
 
 //Retrieve accounts informations
 function getSites(url) {
-  "use strict";
+    "use strict";
     return $.getJSON(url).then(function (data) {
         var founds = [];
         if (data.person) {
@@ -34,14 +33,14 @@ function getSites(url) {
             });
         } else {
             $(".spinner").hide();
-            $("#submit").attr('disabled',false);
+            $("#submit").attr('disabled', false);
             addInfoElement("There are no exact public matches for this email! Try to add your name..");
         }
         return founds;
     })
             .fail(function () {
                 $(".spinner").hide();
-                $("#submit").attr('disabled',false);
+                $("#submit").attr('disabled', false);
                 addInfoElement("Ups! Somenthing went wrong, try again.");
             });
 
@@ -49,31 +48,34 @@ function getSites(url) {
 
 //Create a labelled item depending on the rate
 function addSiteFound(name, domain, rate, info) {
-  "use strict";
+    "use strict";
     var newa = document.createElement("a");
-    switch (rate) {
-        case "easy":
-            newa.className = "list-group-item list-group-success";
-            break;
-        case "hard":
-            newa.className = "list-group-item list-group-item-warning";
-            break;
-        case "impossible":
-            newa.className = "list-group-item list-group-item-danger";
-            break;
-    }
     if (!info) {
         info = "No available information about how to delete from this website";
     }
     newa.innerHTML = "<b>" + name + "</b> " + info + " <span>RATE: " + rate + "</span>";
     newa.href = domain;
-    $(newa).hide().appendTo("#list-group").fadeIn(1000);
+    newa.target = "_blank";
+    switch (rate) {
+        case "easy":
+            newa.className = "list-group-item list-group-success";
+            $(newa).hide().appendTo("#list-good").fadeIn(1000);
+            break;
+        case "hard":
+            newa.className = "list-group-item list-group-item-warning";
+            $(newa).hide().appendTo("#list-medium").fadeIn(1000);
+            break;
+        case "impossible":
+        default:
+            newa.className = "list-group-item list-group-item-danger";
+            $(newa).hide().appendTo("#list-bad").fadeIn(1000);
 
+    }
 }
 
 //Update string parameter, if present
 function updateQueryStringParameter(uri, key, value) {
-  "use strict";
+    "use strict";
     var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
     var separator = uri.indexOf('?') !== -1 ? "&" : "?";
     if (uri.match(re)) {
@@ -83,25 +85,28 @@ function updateQueryStringParameter(uri, key, value) {
         return uri + separator + key + "=" + value;
     }
 }
+
 //Check input and submit
 function checkAndSubmit(event) {
-  "use strict";
+    "use strict";
     event.preventDefault();
-    
-    $("#list-group").empty();
+
+    $("#list-bad").empty();
+    $("#list-medium").empty();
+    $("#list-good").empty();
     $("#info").empty();
     $(".spinner").show();
 
-    var email = $('#inputEmail').val();
-    var name = $('#inputName').val();
-    var last = $('#inputLastname').val();
+    email = $('#inputEmail').val();
+    name = $('#inputName').val();
+    last = $('#inputLastname').val();
     if (email === "") {
         $(".spinner").hide();
-        $("#submit").attr('disabled',false);
+        $("#submit").attr('disabled', false);
         return false;
 
     }
-    $("#submit").attr('disabled',true);
+    $("#submit").attr('disabled', true);
     var piplAPI = 'https://api.pipl.com/search/v4/' + "?email=" + email + "&key=kdrphm4hv87tab5q34v2sf5n" + "&callback=?";
     if (name) {
         piplAPI = updateQueryStringParameter(piplAPI, "first_name", name);
@@ -115,24 +120,29 @@ function checkAndSubmit(event) {
     return getSites(piplAPI).then(function (founds) {
         var foundsLength = founds.length;
         console.log("FOUNDED: " + foundsLength);
-        addInfoElement("Founded <b>" + foundsLength + " site/s</b> registered under " + "<b>" + email + "</b>");
+        addInfoElement("<b>" + foundsLength + "</b> record/s under " + "<b>" + email + "</b>");
+
         $.getJSON('sites.json').then(function (sitesdata) {
             var current;
             $.each(sitesdata, function (k, v) {
                 current = v;
                 $.each(founds, function (k, domainFounded) {
                     if (domainFounded[1] == current.domains) {
-                        console.log("Match: "+domainFounded[0] + " " + domainFounded[1] + " " + current.difficulty);
+                        console.log("Match: " + domainFounded[0] + " " + domainFounded[1] + " " + current.difficulty);
                         addSiteFound(domainFounded[0] + " - " + domainFounded[1], current.url, current.difficulty, current.notes);
                         founds.pop(domainFounded);
-                        return false;
                     }
                 });
 
             });
+        }).then(function () {
+            $.each(founds, function (k, v) {
+                addSiteFound(v[0], v[2], "UKNOWN", "");
+            });
         });
+        
         $(".spinner").hide();
-        $("#submit").attr('disabled',false);
+        $("#submit").attr('disabled', false);
     });
-     
+   
 }
